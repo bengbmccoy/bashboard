4dd74dd1281957741cabe455fbce0dfe5c54562c
bengbmccoy <benjamingbmccoy@gmail.com>
2020-12-21 19:48:28 +1100

    some work on improving the data model for the heatmaps. Also added some comments to trend_aggregation

--- dash/nfl_dash_v1.py
+++ dash/nfl_dash_v1.py
@@ -6,13 +6,16 @@ statistics that runs un your browser and will eventually be published to
 bashboard.fyi
 
 TODO:
-Copy the existing temperature_dash and dash_heatmap files to add a single heat map
-and a second line graph.
-Add the second dropdown for an offence.
-Build the logic for the second heatmap.
-Figure out the logic to display the new line graph.
-Nice to have - Time slider input
-
+- Create a heatmap class
+- Add Comments
+- Generally tidy and restructure
+- Format the dash webpage to be actually nice
+- Make colours more consistent
+- Nice to have - Time slider input (previous X games)
+- Switch run yards data model to include run gaps and not directions
+- Add the avg yards gained to graph #3
+- Add the weekly form heatmap (Top 8, Top 16, Bottom 16)
+- Deply to firebase
 '''
 
 import dash
@@ -34,15 +37,84 @@ class TeamTrend:
     def get_trend_data(self):
         return pd.read_csv('../data/team_trends.csv', index_col=0)
 
+    def trends_teams(self):
+        return
+
+class Heatmap:
+    def __init__(self):
+        self.off_heatmap_df = self.get_heatmap_data('off', [2019])
+        self.def_heatmap_df = self.get_heatmap_data('def', [2019])
+        self.teams = set(self.off_heatmap_df['Team'].tolist())
+
+        self.labels_dict = dict(x='Play Direction', y='Play Types', color='Ranking')
+        self.x_labels = ['Left', 'Middle', 'Right']
+        self.y_labels = ['Long Pass', 'Short Pass', 'Run']
+
+        self.off_plot_array, self.off_labels_array = self.init_arrays()
+        self.def_plot_array, self.def_labels_array = self.init_arrays()
+
+        self.gen_rankings_tables()
+        # print(self.off_rank_table)
+        # print(self.def_rank_table)
+
+    def get_ranks_matrix(self, team):
+        print(team)
+        print(self.off_rank_table)
+        print(self.off_plot_array)
+
+    def gen_rankings_tables(self):
+        self.off_rank_table = self.off_heatmap_df.copy()
+        for i in list(self.off_heatmap_df.columns):
+            if i not in ['Team', 'Year']:
+                self.off_rank_table.sort_values(by=i, inplace=True, ascending=False)
+                col_name = i + ' rank'
+                self.off_rank_table[col_name] = range(1, 33)
+
+        self.def_rank_table = self.def_heatmap_df.copy()
+        for i in list(self.def_heatmap_df.columns):
+            if i not in ['Team', 'Year']:
+                self.def_rank_table.sort_values(by=i, inplace=True, ascending=False)
+                col_name = i + ' rank'
+                self.def_rank_table[col_name] = range(1, 33)
+
+    def get_heatmap_data(self, off_def, year):
+        '''Get the aggregate offensive/defensive CSVs depending on off_def value,
+        then aggregate based on the year and finally return dataframe'''
+
+        if off_def == 'off':
+            data = pd.read_csv('../data/agg_yards_off.csv')
+        elif off_def == 'def':
+            data = pd.read_csv('../data/agg_yards_def.csv')
+
+        # TODO: be able to aggregate multiple years
+        return data.loc[data['Year'].isin(year)]
+
+    def init_arrays(self):
+        '''Return two empty arrays of dimensions 3x3 to be used for plot colour
+        mapping and label values'''
+
+        plot_array = np.empty([3,3])
+        labels_array = np.empty([3,3])
+
+        return plot_array, labels_array
+
+
 def process_arrays(data, plot_array, labels_array, team):
     '''Fill the plot_array and labels_array arrays by iterating through runs
     short passes and long passes and finding the rank and comparisson to the
     league average.'''
 
-    # Set index to Team
-    data.set_index('Team', inplace=True)
     # print(data)
+    # print(team)
+    # print(plot_array)
+    # print(labels_array)
 
+    # Set index to Team
+    # print('here1.1')
+    # print(data)
+    # print(team)
+    # data.set_index('Team', inplace=True)
+    # print('here1.2')
     # Iterate through matrix rows
     for i in range(3):
         # Iterate through matrix cols
@@ -58,14 +130,14 @@ def process_arrays(data, plot_array, labels_array, team):
                 col_name += 'Pass long '
 
             if j == 0:
-                col_name += 'left'
+                col_name += 'left rank'
             elif j == 1:
-                col_name += 'middle'
+                col_name += 'middle rank'
             elif j == 2:
-                col_name += 'right'
+                col_name += 'right rank'
 
             # sort by col_name
-            data.sort_values(by=col_name, inplace=True)
+            data.sort_values(by=col_name, inplace=True, )
             # get index of team after sorted
             plot_array[i,j] = 32 - data.index.get_loc(team)
             # find league avg of col_name
@@ -76,37 +148,23 @@ def process_arrays(data, plot_array, labels_array, team):
     # return arrays
     return plot_array, labels_array
 
-def init_arrays():
-    '''Return two empty arrays of dimensions 3x3 to be used for plot colour
-    mapping and label values'''
-
-    plot_array = np.empty([3,3])
-    labels_array = np.empty([3,3])
-
-    return plot_array, labels_array
-
-def get_heatmap_data(off_def, year):
-    '''Get the aggregate offensive/defensive CSVs depending on off_def value,
-    then aggregate based on the year and finally return dataframe'''
-
-    if off_def == 'off':
-        data = pd.read_csv('../data/agg_yards_off.csv')
-    elif off_def == 'def':
-        data = pd.read_csv('../data/agg_yards_def.csv')
-
-    # TODO: be able to aggregate multiple years
-    return data.loc[data['Year'].isin(year)]
-
 ##########################################
 ''''''''''''''''''''''''''''''''''''''''''
 ''' ########## START PROGRAM ########## '''
 
-off_heatmap_data = get_heatmap_data('off', [2019])
-def_heatmap_data = get_heatmap_data('def', [2019])
+# off_heatmap_data = get_heatmap_data('off', [2019])
+# def_heatmap_data = get_heatmap_data('def', [2019])
+#
+# teams = set(off_heatmap_data['Team'].tolist())
 
-teams = set(off_heatmap_data['Team'].tolist())
+heatmaps = Heatmap()
 
 trends = TeamTrend()
+
+# print(heatmaps.teams)
+# print(trends)
+
+# print(set(trends.trend_df.index.to_list()))
 # print(teams)
 
 app = dash.Dash(__name__)
@@ -118,7 +176,7 @@ app.layout = html.Div(className='row', children=[
             dcc.Dropdown(
                 id='off_team',
                 options=[{"value": x, "label": x}
-                            for x in teams],
+                            for x in heatmaps.teams], # TODO Set to a teams list in a class
                 value='SEA'
             ),
     ]),
@@ -147,17 +205,21 @@ def update_figures(my_dropdown):
     # create figures to return to the call back
 
     ''' Strengths & Weaknesses Heatmaps #1 & #2'''
-    labels_dict = dict(x='Play Direction', y='Play Types', color='Ranking')
-    x_labels = ['Left', 'Middle', 'Right']
-    y_labels = ['Long Pass', 'Short Pass', 'Run']
-    o_data = off_heatmap_data.copy()
-    d_data = def_heatmap_data.copy()
-    off_plot_array, off_labels_array = init_arrays()
-    def_plot_array, def_labels_array = init_arrays()
-    off_plot_array, off_labels_array = process_arrays(o_data, off_plot_array, off_labels_array, my_dropdown)
-    def_plot_array, def_labels_array = process_arrays(d_data, def_plot_array, def_labels_array, my_dropdown)
-    fig1 = px.imshow(off_plot_array, labels=labels_dict, x=x_labels, y=y_labels, zmax=32, zmin=1, color_continuous_scale='rdylgn_r')
-    fig2 = px.imshow(def_plot_array, labels=labels_dict, x=x_labels, y=y_labels, zmax=32, zmin=1, color_continuous_scale='rdylgn_r')
+
+    # print(heatmaps.off_heatmap_df)
+    # print(heatmaps.def_heatmap_df)
+
+    heatmaps.get_ranks_matrix(my_dropdown)
+
+    off_plot_array, off_labels_array = process_arrays(heatmaps.off_heatmap_df.copy(), heatmaps.off_plot_array, heatmaps.off_labels_array, my_dropdown)
+    def_plot_array, def_labels_array = process_arrays(heatmaps.def_heatmap_df.copy(), heatmaps.def_plot_array, heatmaps.def_labels_array, my_dropdown)
+
+    # print(off_plot_array)
+    # print(def_plot_array)
+
+
+    fig1 = px.imshow(off_plot_array, labels=heatmaps.labels_dict, x=heatmaps.x_labels, y=heatmaps.y_labels, zmax=32, zmin=1, color_continuous_scale='rdylgn_r')
+    fig2 = px.imshow(def_plot_array, labels=heatmaps.labels_dict, x=heatmaps.x_labels, y=heatmaps.y_labels, zmax=32, zmin=1, color_continuous_scale='rdylgn_r')
 
     ''' Weekly Yards Gained by Play Type Bar graph #3 '''
 
@@ -172,8 +234,6 @@ def update_figures(my_dropdown):
     fig4 = px.sunburst(df3, path=['play_type', 'play_direction'], values='yards_gained',
                         color_discrete_map={'run':'green', 'short_pass':'red', 'long_pass':'blue'})
     # fig.show()
-
-
 
     return fig1, fig2, fig3, fig4
 
--- ops/trends_aggregation.py
+++ ops/trends_aggregation.py
@@ -1,7 +1,20 @@
 '''
 Written By: Ben McCoy Dec 2020
 
+This code aggregates the play-by-play data into a concatenated csv whcih can be
+indexed by team, and then displays the weekly play_type and play_direction yards_gained
+and other meta data.
 
+This data is to be used on the nfl dash board, for the yards gained by play_type
+and play type usage.
+
+To run:
+
+$ python trends_aggregation.py
+
+TODO:
+- Add the filename as input from an argument
+- Figure out a strategy for dealing with different seasons
 '''
 
 import pandas as pd
@@ -10,72 +23,104 @@ from math import isnan
 
 def main():
 
+    # get the pbp data
     pbp_data = get_data('../data/reg_pbp.csv')
-    # pbp_data = get_data('../data/sample_pbp.csv')
-    pos_data = get_data('../data/pos_data.csv')
+    # pbp_data = get_data('../data/sample_pbp.csv') # For debugging
 
+    # Get list of teams & remove the nan
     teams = list(set(pbp_data['posteam'].to_list()))
     teams = [x for x in teams if str(x) != 'nan']
+
+    # columns in the save_file csv
     columns = ['game_id', 'game_no', 'opp', 'play_type', 'play_direction', 'yards_gained']
     # play_type = ['game_id', 'opp', 'run_left', 'run_mid', 'run_right', 'short_pass_left', 'short_pass_mid', 'short_pass_right', 'long_pass_left', 'long_pass_mid', 'long_pass_right']
+
+    # get list 1-16
     game_no = list(range(1, 17))
 
+    # init the dataframe
     df1 = pd.DataFrame(columns=columns)
 
+    # iterate through each game
     for h in game_no:
+        # iterate through each play_type
         for i in ['run', 'short_pass', 'long_pass']:
+            # iterate through each play_direction
             for j in ['left', 'middle', 'right']:
+                # add a row
                 df1 = df1.append({'game_id': np.nan, 'game_no': h, 'opp': np.nan, 'play_type': i, 'play_direction': j, 'yards_gained': 0}, ignore_index=True)
 
+    # Used previously, cant remember what for
     # df1.fillna({'yards_gained': 0})
     # df1.fillna({'run_left': 0, 'run_mid': 0, 'run_right': 0, 'short_pass_left': 0, 'short_pass_mid': 0, 'short_pass_right': 0, 'long_pass_left': 0, 'long_pass_mid': 0, 'long_pass_right': 0}, inplace=True)
+
+    # a dictionary to hold all the output dataframes
     df_dict = {}
+    # a dictionary to keep track of game ids
     id_dict = {}
+
+    # for each team, add the empty dataframe to the df_dict with the key as the
+    # team and the value as the dataframe
     for i in teams:
         df_dict[i] = df1.copy()
         id_dict[i] = []
-    # print(id_dict)
+    # print(id_dict) # for debugging
 
 
-    print(df_dict)
+    print(df_dict) # for debugging
+
+    # old data model
     # save_file = process_data_old(pbp_data.copy(), teams, df_dict.copy(), id_dict.copy())
+
+    # current data model
     save_file = process_data_new(pbp_data.copy(), teams, df_dict.copy(), id_dict.copy())
 
-
+    # save the file
     save_csv(save_file, '../data/team_trends.csv')
+
+    ''' DATA MODEL '''
 
 def process_data_new(pbp_copy, teams, df_dict_copy, id_dict_copy):
 
+    # iterate through every single play
     for index, row in pbp_copy.iterrows():
+
+        # ensure that the game is from the 2019 season
         if row.game_date[:4] == '2019' or row.game_date[:4] == '2020':
-            # pass
-            # print(row.game_id, row.home_team, row.away_team)
+
+            # print(row.game_id, row.home_team, row.away_team) # For debugging
+
+            # if the game is not in the id_dict list, append it and add the game
+            # to the next available game number and opp in the df_dict.
             if row.game_id not in id_dict_copy[row.home_team]:
                 id_dict_copy[row.home_team].append(row.game_id)
                 df_dict_copy[row.home_team].loc[(df_dict_copy[row.home_team].game_no) == len(id_dict_copy[row.home_team]), 'game_id'] = row.game_id
                 df_dict_copy[row.home_team].loc[(df_dict_copy[row.home_team].game_no) == len(id_dict_copy[row.home_team]), 'opp'] = row.away_team
-                # df_dict_copy[row.home_team].at[len(id_dict_copy[row.home_team]), 'game_id'] = row.game_id
-                # df_dict_copy[row.home_team].at[len(id_dict_copy[row.home_team]), 'opp'] = row.away_team
                 print(row.home_team, df_dict_copy[row.home_team])
 
+            # Same as above, except for away team
             if row.game_id not in id_dict_copy[row.away_team]:
                 id_dict_copy[row.away_team].append(row.game_id)
                 df_dict_copy[row.away_team].loc[(df_dict_copy[row.away_team].game_no) == len(id_dict_copy[row.away_team]), 'game_id'] = row.game_id
                 df_dict_copy[row.away_team].loc[(df_dict_copy[row.away_team].game_no) == len(id_dict_copy[row.away_team]), 'opp'] = row.home_team
                 print(row.away_team, df_dict_copy[row.away_team])
 
+            # Add the run yards to the correct cell based on play_type, play_direction and game
             if row.play_type == 'run':
                 df_dict_copy[row.posteam].loc[(df_dict_copy[row.posteam].game_id == row.game_id) & (df_dict_copy[row.posteam].play_type == row.play_type) & (df_dict_copy[row.posteam].play_direction == row.run_location), 'yards_gained'] += row.yards_gained
 
+            # as above but with pass yards broken down by short or deep (20 yards)
             if row.play_type == 'pass':
                 if row.pass_length == 'short':
                     df_dict_copy[row.posteam].loc[(df_dict_copy[row.posteam].game_id == row.game_id) & (df_dict_copy[row.posteam].play_type == 'short_pass') & (df_dict_copy[row.posteam].play_direction == row.pass_location), 'yards_gained'] += row.yards_gained
                 if row.pass_length == 'deep':
                     df_dict_copy[row.posteam].loc[(df_dict_copy[row.posteam].game_id == row.game_id) & (df_dict_copy[row.posteam].play_type == 'long_pass') & (df_dict_copy[row.posteam].play_direction == row.pass_location), 'yards_gained'] += row.yards_gained
 
-    for k, v in df_dict_copy.items():
-        print(k, v)
+    # For debugging
+    # for k, v in df_dict_copy.items():
+    #     print(k, v)
 
+    # Concat each dataframe int eh dict and return
     return (pd.concat(df_dict_copy.values(), keys=df_dict_copy.keys()))
 
 
